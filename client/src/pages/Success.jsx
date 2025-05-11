73027ab11bc67947d4e86cbc8d81e853bf3cff22
Amir Hamza <amirhamza27940@gmail.com>
2025-05-11 13:19:55 +0600

    success page redesign

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom';
+import image from "../assets/order-complete.gif"
 
 const Success = () => {
     const location = useLocation();
@@ -8,11 +9,18 @@ const Success = () => {
     return (
         <section className="mt-2 bg-white">
             <div className="container mx-auto w-full flex justify-center">
-                <div className="w-full flex-col gap-4 rounded max-w-lg flex p-4 items-center justify-center bg-green-200">
+                <div className="w-full flex-col gap-1 rounded  md:max-w-full max-w-lg flex p-2 items-center justify-center">
                     <p className="font-semibold text-green-800 text-xl text-center">
-                        {Boolean(location.state?.text) ? location.state?.text : "Payment"} has been prossed successfully!
+                        {Boolean(location.state?.text) ? location.state?.text : "Payment"} has been processed successfully!
                     </p>
-                    <Link to={"/"} className='border py-1 px-2 rounded text-green-900 hover:text-white hover:bg-green-900 transition-colors duration-200 font-medium'>Go to Home</Link>
+                    <h2 className='text-center text-base mt-2 font-semibold text-orange-800 ' >Your Order is on it&apos;s way 🏃‍♂️</h2>
+                    <div className="w-full max-h-88 flex justify-center">
+                        <img src={image} alt="" className='w-full h-full object-fill'/>
+                    </div>
+                   <div className="flex justify-between my-2 w-full max-w-lg gap-5">
+                     <Link to={"/"} className='border py-1 px-2 rounded text-green-900 hover:text-white hover:bg-green-900 transition-colors duration-200 font-medium'>Go to Home</Link>
+                     <Link to={"/dashboard/my-orders"} className='border py-1 px-2 rounded text-orange-600 hover:text-white hover:bg-orange-600 transition-colors duration-200 font-medium'>View Your Orders</Link>
+                   </div>
                 </div>
             </div>
         </section>
