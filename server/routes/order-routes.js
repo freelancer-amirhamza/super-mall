ec93e3909c4b354eabac696c57966fa0d66c0b12
Amir Hamza <amirhamza27940@gmail.com>
2025-05-20 23:26:53 +0600

    remove auth

--- server/routes/order-routes.js
+++ server/routes/order-routes.js
@@ -16,9 +16,9 @@ const router = express.Router();
 
 router.post("/cash-on-delivery", auth, cashOnDeliveryOrder);
 router.post("/online-payment", auth,  onlinePayment);
-router.post("/payment-success",auth, handleSuccess);
-router.post("/payment-fail",auth, handleFailed);
-router.post("/payment-cancel",auth, handleCancel);
+router.post("/payment-success", handleSuccess);
+router.post("/payment-fail", handleFailed);
+router.post("/payment-cancel", handleCancel);
 router.get("/get-order-details", auth, getOrderDetails);
 router.get("/get-admin-orders-details", auth, admin, getAdminOrdersDetails);
 router.put("/update-admin-order", auth, admin, updateAdminOrder);
